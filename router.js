d2a5811e2f488724ff0effb3f15238888101d77e
Mark <mark@olechconsulting.com>
2019-05-19 13:21:46 -0400

    Removed some old code from router.js that wasn't required to clean up.

--- router.js
+++ router.js
@@ -2,13 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  
-  let leafs = [
-    { name: 'Matthews', position: 'Center'},
-    { name: 'Marner', position: 'Right Wing'}
-  ];
-  leafs = JSON.stringify(leafs)
-  res.send(leafs);
+  //Stuff to come
 })
 
 module.exports = router;
